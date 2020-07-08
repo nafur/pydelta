42f1fab6e35e766a6b0bece51a6a161f5444656b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-08 16:14:11 +0200

    Adding main file.

---
+++ pydelta.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import pprint
+import sys
+
+import checker
+import options
+import manager
+import mutator
+import parser
+
+args = options.args()
+
+logging.basicConfig(format = '[pydelta %(levelname)s] %(message)s')
+if args.verbose:
+    logging.getLogger().setLevel(level=logging.DEBUG)
+else:
+    logging.getLogger().setLevel(level=logging.INFO)
+
+
+if not os.path.isfile(args.inputfile):
+    raise Exception('Input file is not a regular file')
+
+if args.parse_only:
+    exprs = parser.parse_smtlib(open(args.inputfile).read())
+    pprint.pprint(exprs)
+    sys.exit(0)
+
+if len(args.cmd) == 0:
+    raise Exception('No executable was specified as command')
+if not os.path.isfile(args.cmd[0]):
+    raise Exception('Command "{}" is not a regular file'.format(args.cmd[0]))
+if not os.access(args.cmd[0], os.X_OK):
+    raise Exception('Command "{}" is not executable'.format(args.cmd[0]))
+
+mutator.collect_mutators(args)
+
+checker.compute_golden(args.cmd, args.inputfile)
+
+exprs = parser.parse_smtlib(open(args.inputfile).read())
+
+skip = 0
+m = manager.Manager()
+while True:
+    try:
+        simp = m.simplify(exprs, skip)
+    except KeyboardInterrupt:
+        logging.warning('Aborting. See {} for results.'.format(args.outputfile))
+        break
+    if simp == None:
+        logging.info('No further simplification found')
+        if skip > 0:
+            skip = 0
+            logging.info('Starting over')
+        else:
+            logging.info('Final: {}'.format(parser.render_smtlib(exprs)))
+            break
+    else:
+        logging.info('Simplified: {}'.format(simp.simplification))
+        skip = simp.counter
+        exprs = simp.exprs
+        parser.write_smtlib_to_file(exprs, args.outputfile)
