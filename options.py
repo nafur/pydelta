7de3d077ee80c9d983ecf2856ceb81c5750cd1c5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-21 21:43:18 +0200

    Shuffle options.

--- options.py
+++ options.py
@@ -19,18 +19,21 @@ class OptionParser:
         usage = "{} [<options>] <inputfile> <cmd> [<cmd options>]".format(sys.argv[0])
 
         self.argp = argparse.ArgumentParser(usage = usage, formatter_class = CustomFormatter)
-
         self.argp.add_argument('inputfile', help = 'input file (in SMT-LIB v2 format)')
         self.argp.add_argument('cmd', nargs = argparse.REMAINDER, help = 'the command (with optional arguments)')
-        self.argp.add_argument('--timeout', type = int, metavar = 'seconds', default = 0, help = 'timeout for individual checks')
-        self.argp.add_argument('--max-threads', type = int, metavar = 'n', default = '-1', help = 'number of threads to use; #processors+n if n<=0')
 
-        self.argp.add_argument('--parse-only', action = 'store_true', help = 'only parse the input file')
         self.argp.add_argument('-v', '--verbose', action='store_true', help = 'be more verbose')
         self.argp.add_argument('--version', action = 'version', version = __version__)
-        self.argp.add_argument('--outputfile', metavar = 'filename', default = 'delta.out.smt2', help = 'filename for the output file')
-        self.argp.add_argument('--pretty-print', action = 'store_true', help = 'pretty-print to output file')
-        self.argp.add_argument('--wrap-lines', action = 'store_true', help = 'wrap lines in output file')
+
+        self.argp_checking = self.argp.add_argument_group('checking arguments')
+        self.argp_checking.add_argument('--parse-only', action = 'store_true', help = 'only parse the input file')
+        self.argp_checking.add_argument('--max-threads', type = int, metavar = 'n', default = '-1', help = 'number of threads to use; #processors+n if n<=0')
+        self.argp_checking.add_argument('--timeout', type = int, metavar = 'seconds', default = 0, help = 'timeout for individual checks')
+
+        self.argp_output = self.argp.add_argument_group('output arguments')
+        self.argp_output.add_argument('--outputfile', metavar = 'filename', default = 'delta.out.smt2', help = 'filename for the output file')
+        self.argp_output.add_argument('--pretty-print', action = 'store_true', help = 'pretty-print to output file')
+        self.argp_output.add_argument('--wrap-lines', action = 'store_true', help = 'wrap lines in output file')
 
         self.argp_comparator = self.argp.add_argument_group('comparator arguments')
         self.argp_comparator.add_argument('--ignore-output', action = 'store_true', help = 'ignore stdout and stderr when comparing results')
