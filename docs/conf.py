794248a9b9fe433610fc7cda10dece99f6244cfc
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-23 11:31:51 +0200

    Switch sphinx theme.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -40,6 +42,7 @@ release = ''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 autodoc_default_options = {
@@ -80,13 +83,17 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': True,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
