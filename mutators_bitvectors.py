46ae1672efa3abb3c187e7a33af6274a95f9ca22
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-07-30 08:31:58 +0200

    Added mutator to simplify bitvector constants.

--- mutators_bitvectors.py
+++ mutators_bitvectors.py
@@ -26,9 +26,21 @@ def is_bitvector(node):
     return False
 
 def is_bitvector_constant(node):
-    if is_leaf(node) or len(node) != 3: return False
+    if is_leaf(node):
+        if node.startswith('#b'): return True
+        if node.startswith('#x'): return True
+        return False
+    if len(node) != 3: return False
     if not has_name(node) or get_name(node) != '_': return False
     return node[1].startswith('bv')
+
+def get_bitvector_constant_value(node):
+    assert is_bitvector_constant(node)
+    if is_leaf(node):
+        if node.startswith('#b'): return (int(node[2:], 2), len(node[2:]))
+        if node.startswith('#x'): return (int(node[2:], 16), len(node[2:]) * 4)
+        assert False
+    return (int(node[1][2:]), node[2])
 
 def possible_bitvector_widths_imp(definition):
     if is_bitvector_type(definition):
@@ -54,20 +66,34 @@ class PassBVExtractConstants:
     def mutations(self, node):
         upper = int(node[0][2])
         lower = int(node[0][3])
-        constant = int(node[1][1][2:])
+        constant = get_bitvector_constant_value(node[1])[0]
         constant = constant % (2**(upper+1))
         constant -= constant % (2**lower)
         return [['_', 'bv{}'.format(constant), str(upper - lower + 1)]]
     def __str__(self):
         return 'evaluate bitvector extract on constant'
 
+class PassBVSimplifyConstant:
+    """Replace a constant by a simpler version (smaller fewer bits)."""
+    def filter(self, node):
+        return is_bitvector_constant(node) and get_bitvector_constant_value(node)[0] not in [0, 1]
+    def mutations(self, node):
+        val,width = get_bitvector_constant_value(node)
+        return ['#b{{:0>{}b}}'.format(width).format(v) for v in [val // 2, val // 10]]
+    def __str__(self):
+        return 'simplify bitvector constant'
+
+
 def collect_mutator_options(argparser):
     options.disable_mutator_argument(argparser, 'bitvector', 'bitvector mutators')
     options.disable_mutator_argument(argparser, 'bv-eval-extract', 'evaluate bitvector extract on constants')
+    options.disable_mutator_argument(argparser, 'bv-simplify-constants', 'replaces constants by simpler ones')
 
 def collect_mutators(args):
     res = []
     if args.mutator_bitvector:
         if args.mutator_bv_eval_extract:
             res.append(PassBVExtractConstants())
+        if args.mutator_bv_simplify_constants:
+            res.append(PassBVSimplifyConstant())
     return res
