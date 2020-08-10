404d14a44d704038613a4e60e5eac47b9e167e17
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-08-10 12:00:19 +0200

    Add new mutator to push negations into arithmetic relations.

--- mutators_arithmetic.py
+++ mutators_arithmetic.py
@@ -1,6 +1,11 @@
 import options
 from semantics import *
 
+def is_arithmetic_relation(node):
+    if not has_name(node):
+        return False
+    return get_name(node) in ['=', '<', '>', '>=', '<=', '!=', '<>']
+
 class PassArithmeticSimplifyConstant:
     """Replace a constant by a simpler version (smaller or fewer decimal places)."""
     def filter(self, node):
@@ -14,14 +19,28 @@ class PassArithmeticSimplifyConstant:
     def __str__(self):
         return 'simplify arithmetic constant'
 
+class PassArithmeticNegateRelations:
+    """Replace a constant by a simpler version (smaller or fewer decimal places)."""
+    def filter(self, node):
+        return is_not(node) and is_arithmetic_relation(node[1])
+    def mutations(self, node):
+        negator = { '<': '>=', '<=': '>', '!=': '=', '<>': '=', '>=': '<', '>': '<=' }
+        if node[1][0] in negator:
+            return [negator[node[1][0]]] + node[1][1:]
+        return []
+    def __str__(self):
+        return 'push negations into relations'
 
 def collect_mutator_options(argparser):
     options.disable_mutator_argument(argparser, 'arithmetic', 'arithmetic mutators')
     options.disable_mutator_argument(argparser, 'arith-constants', 'replaces constants by simpler ones')
+    options.disable_mutator_argument(argparser, 'arith-negate-relations', 'push negations inside of relations')
 
 def collect_mutators(args):
     res = []
     if args.mutator_arithmetic:
         if args.mutator_arith_constants:
             res.append(PassArithmeticSimplifyConstant())
+        if args.mutator_arith_negate_relations:
+            res.append(PassArithmeticNegateRelations())
     return res
