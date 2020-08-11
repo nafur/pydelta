c0e122e18960cea58a851aeeeb03cf99e1e6bef5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-08-11 17:39:17 +0200

    Fixed syntax error.

--- pydelta/info.py
+++ pydelta/info.py
@@ -4,7 +4,7 @@ import subprocess
 PACKAGE_NAME = 'pydelta-smt'
 PROJECT_NAME = 'pyDelta'
 DESCRIPTION = 'Delta debugger for SMT-LIB files'
-VERSION = = subprocess.check_output(['git', 'describe', '--tags']).decode('utf8').strip()
+VERSION = subprocess.check_output(['git', 'describe', '--tags']).decode('utf8').strip()
 PYPI_VERSION = re.sub('v([0-9.]+)-([0-9]+)-g[a-z0-9]+', '\\1.dev\\2', VERSION)
 AUTHOR = 'Gereon Kremer'
 AUTHOR_EMAIL  = 'gereon.kremer@gmail.com'
