db69510d77ed665549e231cfc0aa415744387b8b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-08-11 17:23:39 +0200

    Working on pypi metadata.

--- pydelta/info.py
+++ pydelta/info.py
@@ -1,6 +1,12 @@
+import re
+import subprocess
 
+PACKAGE_NAME = 'pydelta-smt'
 PROJECT_NAME = 'pyDelta'
+DESCRIPTION = 'Delta debugger for SMT-LIB files'
 VERSION = '0.1'
+FULL_VERSION = subprocess.check_output(['git', 'describe', '--tags']).decode('utf8').strip()
+PYPI_VERSION = re.sub('v([0-9.]+)-([0-9]+)-g[a-z0-9]+', '\\1.dev\\2', FULL_VERSION)
 AUTHOR = 'Gereon Kremer'
 AUTHOR_EMAIL  = 'gereon.kremer@gmail.com'
 REPOSITORY = 'https://github.com/nafur/pydelta'
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 from pydelta import info
 
 setup(
-    name = info.PROJECT_NAME,
-    version=info.VERSION,
-    description='Test',
-    url=info.REPOSITORY,
-    author=info.AUTHOR,
-    author_email=info.AUTHOR_EMAIL,
-    license='MIT',
-    packages=['pydelta'],
-    zip_safe=False,
+    name = info.PACKAGE_NAME,
+    version = info.PYPI_VERSION,
+    description = info.DESCRIPTION,
+    url = info.REPOSITORY,
+    author = info.AUTHOR,
+    author_email = info.AUTHOR_EMAIL,
+    license = 'MIT',
+    packages = ['pydelta'],
+    zip_safe = False,
     scripts = [
         'bin/pydelta'
     ]
