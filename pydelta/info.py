cec9ac75a32e373dd6fd618cfca3e2796f0e5255
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-08-11 17:38:18 +0200

    Working on version.

--- pydelta/info.py
+++ pydelta/info.py
@@ -4,9 +4,8 @@ import subprocess
 PACKAGE_NAME = 'pydelta-smt'
 PROJECT_NAME = 'pyDelta'
 DESCRIPTION = 'Delta debugger for SMT-LIB files'
-VERSION = '0.1'
-FULL_VERSION = subprocess.check_output(['git', 'describe', '--tags']).decode('utf8').strip()
-PYPI_VERSION = re.sub('v([0-9.]+)-([0-9]+)-g[a-z0-9]+', '\\1.dev\\2', FULL_VERSION)
+VERSION = = subprocess.check_output(['git', 'describe', '--tags']).decode('utf8').strip()
+PYPI_VERSION = re.sub('v([0-9.]+)-([0-9]+)-g[a-z0-9]+', '\\1.dev\\2', VERSION)
 AUTHOR = 'Gereon Kremer'
 AUTHOR_EMAIL  = 'gereon.kremer@gmail.com'
 REPOSITORY = 'https://github.com/nafur/pydelta'
