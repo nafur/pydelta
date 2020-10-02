7e6dd7bf8a8e3bec7c7057d19ba4cce6d753f646
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-10-02 09:37:50 +0200

    Add a mutator that simplifies the smtlib logic.

--- pydelta/mutators_smtlib.py
+++ pydelta/mutators_smtlib.py
@@ -13,6 +13,21 @@ class PassCheckSatAssuming:
     def __str__(self):
         return 'substitute check-sat-assuming by check-sat'
 
+class PassSimplifyLogic:
+    """Replaces the logic specified in :code`(check-logic ...)` by a simpler one."""
+    def filter(self, node):
+        return has_name(node) and get_name(node) == 'set-logic'
+    def mutations(self, node):
+        logic = node[1]
+        cands = []
+        repls = { 'BV': '', 'FP': '', 'UF': '', 'S': '', 'T': '', 'NRA': 'LRA' }
+        for r in repls:
+            if r in logic:
+                cands.append(logic.replace(r, repls[r]))
+        return [['set-logic', c] for c in cands]
+    def __str__(self):
+        return 'simplify logic'
+
 class PassPushPopRemoval:
     """Removes matching :code:`(push)(pop)` pairs. First tries successive pairs, distant ones later."""
     def filter(self, node):
@@ -47,6 +62,7 @@ def collect_mutator_options(argparser):
     options.add_mutator_argument(argparser, NAME, True, 'smtlib mutators')
     options.add_mutator_argument(argparser, 'check-sat-assuming', True, 'replace check-sat-assuming by check-sat')
     options.add_mutator_argument(argparser, 'push-pop-removal', True, 'remove push-pop pairs')
+    options.add_mutator_argument(argparser, 'simplify-logic', True, 'simplify declared logic')
 
 def collect_mutators(args):
     res = []
@@ -55,4 +71,6 @@ def collect_mutators(args):
             res.append(PassCheckSatAssuming())
         if args.mutator_push_pop_removal:
             res.append(PassPushPopRemoval())
+        if args.mutator_simplify_logic:
+            res.append(PassSimplifyLogic())
     return res
